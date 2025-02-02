48ecb09a526368ffae726b77ae12e0b87e8b4c4f
EbukaNweje <nwejeebuka@gmail.com>
2025-02-02 09:18:35 -0800

    D

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -16,7 +16,7 @@ exports.register = async (req, res, next)=>{
       const { email } = req.body;
 
       User.findOne({ email }, async (err, user) => {
-        // console.log(user)
+
         if (err) {
           return res.status(500).json({ error: err.message });
         }
